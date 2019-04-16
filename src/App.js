572e542ce3c94814e8862139d85b531b624b8e8d
cliYao <568077487@qq.com>
2019-04-16 16:06:13 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './common/header';
 import Home from './pages/home';
 import Detail from './pages/detail/loadable.js';
@@ -11,24 +11,24 @@ import {Globalstyle} from './style.js'
 
 
 class App extends Component {
-  render() {
-    return (
-    	<Provider store={store}>
-    		<div>
-	      		<Globalstyle />
-	      		<BrowserRouter>
-	      			<div>
-	      				<Header />
-		      			<Route path='/' exact component={Home}></Route>
-		      			<Route path='/login' exact component={Login}></Route>
-		      			<Route path='/write' exact component={Write}></Route>
-		      			<Route path='/detail/:id' exact component={Detail}></Route>
-	      			</div>
-	      		</BrowserRouter>
-      		</div>
-      	</Provider>
-    )
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <div>
+                    <Globalstyle/>
+                    <BrowserRouter>
+                        <div>
+                            <Header/>
+                            <Route path='/' exact component={Home}></Route>
+                            <Route path='/login' exact component={Login}></Route>
+                            <Route path='/write' exact component={Write}></Route>
+                            <Route path='/detail/:id' exact component={Detail}></Route>
+                        </div>
+                    </BrowserRouter>
+                </div>
+            </Provider>
+        )
+    }
 }
 
 export default App;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,40 +1,44 @@
-import React,{ PureComponent } from 'react';
-import { Redirect } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { LoginWrapper,LoginBox,Input,Button} from './style'
-import {actionCreators } from './store'
+import React, {PureComponent} from 'react';
+import {Redirect} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {LoginWrapper, LoginBox, Input, Button} from './style'
+import {actionCreators} from './store'
 
 class Login extends PureComponent {
-	render() {
-		const {loginStatus} = this.props
-		console.log(loginStatus)
-		if(!loginStatus){
-			return (
-				<LoginWrapper>
-					<LoginBox>
-						<Input placeholder='账号' ref={(input)=>{this.account = input}}/>
-						<Input placeholder='密码' type='password' ref={(input)=>{this.password = input}}/>
-						<Button onClick={()=>this.props.login(this.account,this.password)}>登陆</Button>
-					</LoginBox>
-				</LoginWrapper>
-			)
-		}else{
-			return <Redirect to='/'/>
-		}
-		
-	}
+    render() {
+        const {loginStatus} = this.props
+        console.log(loginStatus)
+        if (!loginStatus) {
+            return (
+                <LoginWrapper>
+                    <LoginBox>
+                        <Input placeholder='账号' ref={(input) => {
+                            this.account = input
+                        }}/>
+                        <Input placeholder='密码' type='password' ref={(input) => {
+                            this.password = input
+                        }}/>
+                        <Button onClick={() => this.props.login(this.account, this.password)}>登陆</Button>
+                    </LoginBox>
+                </LoginWrapper>
+            )
+        } else {
+            return <Redirect to='/'/>
+        }
+
+    }
 }
 
 const mapState = (state) => ({
-	loginStatus:state.getIn(['login','login'])
+    loginStatus: state.getIn(['login', 'login'])
 })
 
 
-const mapDispatch =(dispatch) =>({
-	login(accountElem,passwordElem){
-		const action=actionCreators.login()
-		dispatch(action)
-	}
+const mapDispatch = (dispatch) => ({
+    login(accountElem, passwordElem) {
+        const action = actionCreators.login()
+        dispatch(action)
+    }
 })
 
-export default connect(mapState,mapDispatch)(Login);
+export default connect(mapState, mapDispatch)(Login);
